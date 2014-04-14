47951de8d437de46307074f7998c2bdb757a33ea
nkosi91 <joy.hlatshwayo@gmail.com>
2014-04-15 01:11:15 +0200

    finishing up

--- question7.py
+++ question7.py
@@ -1,2 +1,11 @@
 #QUESTION7
-import
+#receives 2 arguments from the command line: the path of fasta file that contains a single DNA sequence, and the path of a new file where the program will save the translate protein. Make sure to verify the correct ORF and start & stop of the sequence
+import question1_six
+def multifun(source, save):
+    question1_six.read_fasta_file(source)
+    question1_six.get_ORF(idandseq[2])
+    question1_six.get_gene_by_ORF(ORF)
+    question1_six.translate(gene)
+    question1_six.createfasta(save)
+
+print multifun()
